3b7b9848c5ba097ff0eb9fb6b89207b982fe708a
mogh77 <mogh771377@gmail.com>
2016-01-04 08:46:52 -0600

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -217,7 +217,7 @@ function create_config( )
   "plugins",
   "stats",
   "version"},
-  sudo_users = {140402412},
+  sudo_users = {176894771,120518968},
   disabled_channels = {},
   moderation = {data = 'data/moderation.json'}
   }
